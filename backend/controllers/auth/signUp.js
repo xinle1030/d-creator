66cc79e4809d689460947aa61415ea8b1e7ae8b6
Lam Xin Le <xinlelam1030@gmail.com>
2023-02-26 06:57:15 +0800

    complete auth controllers

---
+++ backend/controllers/auth/signUp.js
@@ -0,0 +1,25 @@
+const User = require("../../models/User");
+var bcrypt = require("bcryptjs");
+
+module.exports = (req, res) => {
+
+  console.log("BE: sign up")
+
+  // create new User
+  const user = new User({
+    username: req.body.username,
+    name: req.body.name,
+    email: req.body.email,
+    password: bcrypt.hashSync(req.body.password, 8),
+    role: req.body.role,
+  });
+
+  // save to db
+  user.save((err, user) => {
+    if (err) {
+      res.status(500).send({ message: err });
+      return;
+    }
+    res.send(user);
+  });
+};
