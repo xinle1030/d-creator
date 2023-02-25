5936cbea125bf24e6d12d1b600240111dda5bdc5
Lam Xin Le <xinlelam1030@gmail.com>
2023-02-26 06:57:55 +0800

    complete User Model

---
+++ backend/models/User.js
@@ -0,0 +1,30 @@
+const mongoose = require("mongoose");
+
+const userSchema = new mongoose.Schema({
+  name: {
+    type: String,
+    required: true,
+  },
+  username: {
+    type: String,
+    required: true,
+    unique: true,
+  },
+  email: {
+    type: String,
+    required: true,
+    unique: true,
+  },
+  password: {
+    type: String,
+    required: true,
+  },
+  role: {
+    type: String,
+    required: true,
+      default: 'creator',
+      enum: ["creator", "advertiser", "admin"]
+  }
+});
+
+module.exports = mongoose.model("User", userSchema);
