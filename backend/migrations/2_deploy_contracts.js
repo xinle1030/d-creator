9461972684ed892390718c281ff7469acb8afee6
Lam Xin Le <xinlelam1030@gmail.com>
2023-02-26 11:10:06 +0800

    transfer to address during migration

--- backend/migrations/2_deploy_contracts.js
+++ backend/migrations/2_deploy_contracts.js
@@ -4,6 +4,13 @@ const InfluencerMarketingContract = artifacts.require(
 const KOLCoin = artifacts.require("./KOLCoin.sol");
 
 module.exports = async function (deployer) {
-  await deployer.deploy(InfluencerMarketingContract);
   await deployer.deploy(KOLCoin);
+  const kolCoin = await KOLCoin.deployed();
+
+  await deployer.deploy(InfluencerMarketingContract, kolCoin.address);
+  const influencerContract = await InfluencerMarketingContract.deployed();
+
+  const addr1 = '0x518D2976650A28F45900DB68DAD4d74926e4F4F9';
+
+  await kolCoin.transfer(addr1, "100000000000000000000");
 };
