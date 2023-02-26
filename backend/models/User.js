fa7c0d02089337f7b46aea0d9c97c2566fbc1f8c
Lam Xin Le <xinlelam1030@gmail.com>
2023-02-26 11:10:26 +0800

    add walletAdrHash to User model

--- backend/models/User.js
+++ backend/models/User.js
@@ -19,6 +19,9 @@ const userSchema = new mongoose.Schema({
     type: String,
     required: true,
   },
+  walletAdrHash: {
+    type: String,
+  },
   role: {
     type: String,
     required: true,
